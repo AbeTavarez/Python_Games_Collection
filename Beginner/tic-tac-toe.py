ed7958f700d9352c8b9daf13ce1040960daadf75
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-08 22:46:30 -0500

    init tic tac toe

--- Beginner/tic-tac-toe.py
+++ Beginner/tic-tac-toe.py
@@ -2,6 +2,14 @@
 
 
 # Create the game board
-game_board = [[0, 0, 0, ], [0, 0, 0], [0, 0, 0]]
+game_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
 # Let's use list filled with number zeros to create the board
-print(game_board)
+def print_board():
+    print(game_board[0])
+    print(game_board[1])
+    print(game_board[2])
+
+
+print_board()
